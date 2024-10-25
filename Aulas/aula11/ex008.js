6ae3af468a4e455cf9a659581ab53c65ab8813ca
Carlos Eduardo <carloseduardomveloso@gmail.com>
2024-10-25 09:59:19 -0300

    começando aulas de condições

---
+++ Aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.1
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+if (vel > 50) { //Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
+console.log(`Diriga sempre usando cinto de segurança!`);
---
+++ Aulas/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = "Brasil"
+console.log(`Vivendo em ${pais}`) 
+if (pais == "Brasil") { //Condição Composta
+    console.log(`Você é brasileiro!`);
+}else{
+    console.log(`Você é estrangeiro!`);
+}
