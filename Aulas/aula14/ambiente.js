dcc89382602807d968a48a4fc7a59d063c02315b
Carlos Eduardo <143442055+Carlosmveloso@users.noreply.github.com>
2024-11-07 15:48:34 -0300

    finalizando aula de repetição

---
+++ Aulas/aula14/ambiente.js
@@ -0,0 +1,13 @@
+/*var contador = 1
+
+while ( contador <= 10) {
+    console.log(`Repetição ${contador}`);
+    contador++
+}*/
+
+// Como for podemor fazer a mesma coisa que o while e do while fazem:
+
+for (var contador = 1; contador <= 10; contador++) {
+    console.log(`Repetição ${contador}`);
+    
+}
