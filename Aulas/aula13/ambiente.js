ab4481af5d586d9e7d1a44c7bbdb2ceaca8d42b9
Carlos Eduardo <143442055+Carlosmveloso@users.noreply.github.com>
2024-11-06 21:02:49 -0300

    inicio das aulas sobre estrutura de repetição

---
+++ Aulas/aula13/ambiente.js
@@ -0,0 +1,11 @@
+//console.log("Tudo bem ?");
+//console.log("Tudo bem ?");
+//console.log("Tudo bem ?");
+
+
+// Estrutura de repetição com teste lógico no início
+var contador = 1
+while (contador <= 3) {
+    console.log(`Contador ${contador}`);
+    contador++
+}
---
+++ Aulas/aula13/ambiente2.js
@@ -0,0 +1,8 @@
+//Estrura de repetição com teste lógico no final
+
+
+var contador = 1
+do {
+    console.log(`Passo ${contador}`);
+    contador++
+} while (contador <= 3)
