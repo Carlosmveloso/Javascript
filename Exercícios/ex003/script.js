6b080964a25d098f00b823be0bbd17b58200eb6c
Carlos Eduardo <143442055+Carlosmveloso@users.noreply.github.com>
2024-11-08 12:20:18 -0300

    Exercício 03 concluído

--- Exercícios/ex003/script.js
+++ Exercícios/ex003/script.js
@@ -1,45 +1,35 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = window.document.getElementById("txtano")
-    var res = window.document.querySelector("div#res")
+function contar() {
+    let inicio = window.document.getElementById("txti")
+    let fim = window.document.getElementById("txtf")
+    let passo = window.document.getElementById("txtp")
+    let resultado = window.document.getElementById("res")
 
-    if (fano.value.length == 0 || Number(fano.value) > ano ) {
-        window.alert(`[ERRO] Verifique os dados e tente novamente!`)
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        //window.alert("[ERRO] faltam dados para podermos prosseguir .")
+        resultado.innerHTML = "Impossível contar !"
     } else {
-        var fsex =  document.getElementsByName("radsex")
-        var idade = ano - Number(fano.value)
-        var genero = ""
-        var img = document.createElement("img")
-        img.setAttribute("id", "foto")
+        resultado.innerHTML = `Contando: </br>`
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
-        if (fsex[0].checked) {
-            genero = "Homem"
-            if (idade >= 0 && idade < 18) {
-                //Criança/Adolecente
-                img.setAttribute("src", "imagens/menino.jpg")
-            } else if (idade >= 18 && idade < 65) {
-                //Adulto
-                img.setAttribute("src", "imagens/homem.jpg")
-            } else {
-                //Idoso
-                img.setAttribute("src", "imagens/idoso.jpg")
-            }
-        } else if (fsex[1].checked) {
-            genero = "Mulher"
-            if (idade >= 0 && idade < 18) {
-                //Criança/Adolecente
-                img.setAttribute("src", "imagens/menina.jpg")
-            } else if (idade >= 18 && idade < 65) {
-                //Adulta
-                img.setAttribute("src", "imagens/mulher.jpg")
-            } else {
-                //Idosa
-                img.setAttribute("src", "imagens/idosa.jpg")
-            }
+        if (p <= 0) {
+            window.alert("Passo inválido! Considerando PASSO 1")
+            p = 1
         }
-        res.style.textAlign = "center"
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img) 
+
+        if (i < f ) {
+            //Contagem Crescente
+            for (let c = i; c <= f; c += p) {
+                resultado.innerHTML += `${c} \u{1F449} `
+            }
+            resultado.innerHTML += `\u{1F6A9}`
+        } else {
+            //Contagem Decrecente
+            for (let c = i; c >= f; c -= p) {
+                resultado.innerHTML += `${c} \u{1F449} `
+            }
+             resultado.innerHTML += `\u{1F6A9}`
+        }
     }
 }
