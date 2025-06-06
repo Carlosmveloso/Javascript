54675674e7e8bb62530f02c34fc7c02c10fa576a
Carlos Eduardo <carloseduardomveloso@gmail.com>
2025-06-06 00:39:22 -0300

    praticando javascript denovo

--- Exercícios/ex002/script.js
+++ Exercícios/ex002/script.js
@@ -1,45 +1,51 @@
+let verificarIdade = document.getElementById("verificar");
+verificarIdade.addEventListener("click", verificar);
+
 function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = window.document.getElementById("txtano")
-    var res = window.document.querySelector("div#res")
+  let dataAtual = new Date();
+  let anoAtual = dataAtual.getFullYear();
+  let anoDoUsuario = document.getElementById("textoAno");
+  let valorAnoDoUsuario = anoDoUsuario.value;
+  let resultado = document.getElementById("res");
 
-    if (fano.value.length == 0 || Number(fano.value) > ano ) {
-        window.alert(`[ERRO] Verifique os dados e tente novamente!`)
+  if (valorAnoDoUsuario.length == 0 || valorAnoDoUsuario > anoAtual) {
+    alert("Verifique os dados novamente");
+  } else {
+    let femaleSex = document.getElementById("femaleBox");
+    let maleSex = document.getElementById("maleBox");
+    let idade = anoAtual - Number(valorAnoDoUsuario);
+    let genero = "";
+    let img = document.createElement("img");
+    img.setAttribute("id", "foto");
+
+    if (idade < 18 && maleSex.checked) {
+      genero = "Menino";
+      resultado.innerHTML = `Foi registrado um ${genero} com ${idade} anos de idade.`;
+      img.setAttribute("src", "./imagens/menino.jpg");
+    } else if (idade < 18 && femaleSex.checked) {
+      genero = "Menina";
+      resultado.innerHTML = `Foi registrado uma ${genero} com ${idade} anos de idade.`;
+      img.setAttribute("src", "./imagens/menina.jpg");
+    } else if (idade >= 18 && idade <= 65 && maleBox.checked) {
+      genero = "Homem";
+      resultado.innerHTML = `Foi registrado um ${genero} com ${idade} anos de idade.`;
+      img.setAttribute("src", "./imagens/homem.jpg");
+    } else if (idade >= 18 && idade <= 65 && femaleBox.checked) {
+      genero = "Mulher";
+      resultado.innerHTML = `Foi registrado uma ${genero} com ${idade} anos de idade.`;
+      img.setAttribute("src", "./imagens/mulher.jpg");
+    } else if (idade > 65 && idade < 101 && maleBox.checked) {
+      genero = "Senhor";
+      resultado.innerHTML = `Foi registrado um ${genero} com ${idade} anos de idade.`;
+      img.setAttribute("src", "./imagens/idoso.jpg");
+    } else if (idade > 65 && idade < 101 && femaleBox.checked) {
+      genero = "Senhora";
+      resultado.innerHTML = `Foi registrado uma ${genero} com ${idade} anos de idade.`;
+      img.setAttribute("src", "./imagens/idosa.jpg");
     } else {
-        var fsex =  document.getElementsByName("radsex")
-        var idade = ano - Number(fano.value)
-        var genero = ""
-        var img = document.createElement("img")
-        img.setAttribute("id", "foto")
-
-        if (fsex[0].checked) {
-            genero = "Homem"
-            if (idade >= 0 && idade < 18) {
-                //Criança/Adolecente
-                img.setAttribute("src", "imagens/menino.jpg")
-            } else if (idade >= 18 && idade < 65) {
-                //Adulto
-                img.setAttribute("src", "imagens/homem.jpg")
-            } else {
-                //Idoso
-                img.setAttribute("src", "imagens/idoso.jpg")
-            }
-        } else if (fsex[1].checked) {
-            genero = "Mulher"
-            if (idade >= 0 && idade < 18) {
-                //Criança/Adolecente
-                img.setAttribute("src", "imagens/menina.jpg")
-            } else if (idade >= 18 && idade < 65) {
-                //Adulta
-                img.setAttribute("src", "imagens/mulher.jpg")
-            } else {
-                //Idosa
-                img.setAttribute("src", "imagens/idosa.jpg")
-            }
-        }
-        res.style.textAlign = "center"
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img) 
+      resultado.innerHTML = `Parabens pela sua idade de ${idade} anos.`;
     }
+    resultado.style.textAlign = 'center';
+    resultado.appendChild(img)
+  }
 }
